04efce3f89ee8b174068f6276c3a5f17b5f0723d
lestmrp <92368889+lestmrp@users.noreply.github.com>
2021-10-12 19:28:06 +0700

    Add files via upload

---
+++ Java1.java
@@ -0,0 +1,18 @@
+public class Java1 {
+
+public static void main(String[] args) {
+    String first_name, lastName, phoneNumber, address, hobby;
+    int age;
+
+    first_name = "Juniathami";
+    lastName = "Lestari";
+    phoneNumber = "082164833073";
+    address = "Tebing Tinggi";
+    age = 23;
+    hobby = "Membaca";
+
+    System.out.println("Name   :" + first_name + lastName + "\n" + "age    :" + age + "Tahun" + "\n" + "Phone  :"
+                + phoneNumber + "\n" + "Address:" + address + "\n" + "Hobi   :" + hobby);
+
+}
+}
